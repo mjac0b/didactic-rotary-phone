09234541d90719545c6a2324588a0d6b2f2cfa9f
Michael Jacob <46906037+mjac0b@users.noreply.github.com>
2019-03-11 20:11:34 +0000

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,25 @@
+#function declaring genre, artist & year
+
+#genre function definition
+def genre ():
+    print ("Gospel")
+genre()
+
+#artist function definition
+def artist ():
+    print ("Eben")
+artist()
+
+#year function
+def year():
+    print(2018)
+year()
+
+#boolean function
+def albumYear():
+    output = 2019
+    year = 2018
+    if output == year:
+        print (True)
+    if output > year:
+        print(False)
